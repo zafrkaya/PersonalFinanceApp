7a8ab8ab0b5d2a49ff890c65ca9b4ca63e82d691
zafrkaya <zafonacci@gmail.com>
2024-06-18 20:39:57 +0300

    webhook test

--- PersonalFinance.API/Controllers/WeatherForecastController.cs
+++ PersonalFinance.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace PersonalFinance.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Webhook Test!", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
